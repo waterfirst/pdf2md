06b36a991694d823ea54adac62b2ff80fba1f3ad
waterfirst <nakcho.choi@gmail.com>
2025-03-15 22:35:47 +0900

    Fix hydration warning in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body className={`antialiased overflow-y-hidden`}>{children}</body>
+      <body suppressHydrationWarning className={`antialiased overflow-y-hidden`}>{children}</body>
     </html>
   );
 }
